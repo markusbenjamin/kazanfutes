fd1f109e4650ad91115a17a2ea28343f30616138
markusbenjamin <markus.benjamin@gmail.com>
2024-09-12 20:45:29 +0200

    Error registration tested.

--- dev/dev_scripts/error_registration_tester.py
+++ dev/dev_scripts/error_registration_tester.py
@@ -3,6 +3,6 @@ from utils.project import *
 settings.set('verbosity',True)
 
 def foo():
-    error_registrar(exception_type="Test exception",severity=3)
+    error_registrar(exception_type="Test exception 2",severity=3)
 
 foo()
