a6d5027a68eccb8c51df567fc17a8eba420e88ad
markusbenjamin <markus.benjamin@gmail.com>
2024-09-12 18:36:47 +0200

    Logging interface set up.

--- services/data_uploader.py
+++ services/data_uploader.py
@@ -4,4 +4,4 @@ Syncs the contents of the data folder to the online repo.
 
 from utils.project import *
 
-push_dir_to_repo('data/', 'Automatic data push.')
+sync_dir_with_repo('data/', 'Automatic data push.')
--- services/temperature_and_humidity_logger.py
+++ services/temperature_and_humidity_logger.py
@@ -3,15 +3,6 @@ Logs temperature and humidity values from the rooms as specified in rooms.json.
 """
 
 from utils.project import *
-import logging
-from logging.handlers import TimedRotatingFileHandler
-import json
 
-log_file_path = f'{get_project_root()}/data/logs/temperature_and_humidity/temperature_and_humidity.json'
-handler = TimedRotatingFileHandler(log_file_path, when='midnight', interval=1)
-logging.getLogger().addHandler(handler)
-logging.getLogger().setLevel(logging.INFO)
-
-room_temps_and_humidity = get_room_temps_and_humidity()
-
-logging.info(json.dumps(room_temps_and_humidity))
+init_logger(log_file=f'{get_project_root()}/data/logs/temperature_and_humidity/temperature_and_humidity.json')
+log_data(get_room_temps_and_humidity())
