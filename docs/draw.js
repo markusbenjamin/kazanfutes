0428998991606ee80e2d6106c152766ec45a0b7e
markusbenjamin <markus.benjamin@gmail.com>
2025-01-22 11:33:18 +0100

    Added dev redirect, added gas usage dial to boiler.

--- docs/draw.js
+++ docs/draw.js
@@ -5,6 +5,7 @@ function setup() {
     createCanvas(1, 1, SVG);
     colorMode(RGB, 1, 1, 1, 1);
     rectMode(CENTER);
+    strokeCap(SQUARE);
 }
 
 function draw() {
@@ -14,19 +15,21 @@ function draw() {
 
         //rect(xOffset, yOffset, drawingWidth, drawingHeight);
 
-
+        let maxGasUsage = 7;
+        let mappedGasUsage = constrain(currentGasUsageRate, 0, maxGasUsage) / maxGasUsage;
         if (boilerState != null) {
             const boxDims = getBBoxP5jsDimensions("flame_nest");
             let p = { x: boxDims.cx, y: boxDims.cy }
             let s = mapSize(0.05, 0.025 * aspectRatio);
 
+            let flameSize = 0.5 + mappedGasUsage * 0.7;
             if (boilerState == 1) {
                 let wiggleAmount = 0.06;
                 drawFlame(
                     p.x - boxDims.w * 0.15,
                     p.y + boxDims.h * 0.2,
-                    s.w * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.5,
-                    s.h * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.5,
+                    s.w * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.5 * flameSize,
+                    s.h * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.5 * flameSize,
                     color(1, 0.5, 0, 0.875),
                     color(1, 1, 0, 0.9),
                     true
@@ -34,12 +37,25 @@ function draw() {
                 drawFlame(
                     p.x + boxDims.w * 0.2,
                     p.y + boxDims.h * 0.2,
-                    s.w * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.2,
-                    s.h * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.2,
+                    s.w * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.35 * flameSize,
+                    s.h * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.35 * flameSize,
                     color(1, 0.5, 0, 0.875),
                     color(1, 1, 0, 0.9),
                     true
                 );
+                if (false) {
+                    for (let i = -3; i < 4; i++) {
+                        drawFlame(
+                            p.x + boxDims.w * 0.2,
+                            p.y + boxDims.h * 0.2,
+                            s.w * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.2,
+                            s.h * random(1 - wiggleAmount, 1 + wiggleAmount) * 1.2,
+                            color(1, 0.5, 0, 0.875),
+                            color(1, 1, 0, 0.9),
+                            true
+                        );
+                    }
+                }
             } else {
                 let wiggleAmount = 0.035;
                 for (let i = -3; i < 4; i++) {
@@ -53,6 +69,45 @@ function draw() {
                         true
                     );
                 }
+            }
+            if (isValidNumber(currentGasUsageRate)) {
+                //console.log(currentGasUsageRate);
+                const boilerDims = getBBoxP5jsDimensions("boiler_body");
+
+                let p = { x: boilerDims.cx, y: boilerDims.cy - boilerDims.h * 0.2 };
+                let s = boilerDims.w * 0.5;
+                stroke(boilerState ? 1 : 0, 0, boilerState ? 0 : 1, 1);
+                stroke(0);
+                strokeWeight(0.6);
+                noFill();
+                ellipse(p.x, p.y, s, s);
+                fill(0);
+                ellipse(p.x, p.y, s * 0.1, s * 0.1);
+
+
+                let gasUsageDialOffset = PI / 3;
+                let gasUsageDialSpacing = PI / 6;
+
+                let dialAngle = PI / 2 + gasUsageDialOffset + (TWO_PI - 2 * gasUsageDialOffset) * mappedGasUsage;
+                let dialLength = s * 0.038;
+
+                let tickStartLength = s * 0.038 * 0.8;
+                for (let tickAngle = PI / 2 + gasUsageDialOffset; tickAngle <= PI / 2 + TWO_PI - gasUsageDialOffset + gasUsageDialSpacing; tickAngle += gasUsageDialSpacing) {
+                    strokeWeight(0.25);
+                    stroke(0.5);
+                    line(
+                        p.x + s * tickStartLength * cos(tickAngle),
+                        p.y + s * tickStartLength * sin(tickAngle),
+                        p.x + s * dialLength * cos(tickAngle),
+                        p.y + s * dialLength * sin(tickAngle)
+                    );
+                }
+                strokeWeight(0.5);
+                stroke(0.1);
+                strokeCap(ROUND);
+                line(p.x, p.y, p.x + s * dialLength * cos(dialAngle), p.y + s * dialLength * sin(dialAngle));
+                strokeCap(SQUARE);
+
             }
         }
 
@@ -115,7 +170,7 @@ function getBBoxRelativeDimensions(id) {
 }
 
 function getBBoxDrawingDimensions(id) {
-    const d3Dims =  d3.select("#" + id).node().getBBox();
+    const d3Dims = d3.select("#" + id).node().getBBox();
     return {
         x: d3Dims.x,
         y: d3Dims.y,
