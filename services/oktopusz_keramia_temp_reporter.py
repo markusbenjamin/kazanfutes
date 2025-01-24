c1422cb63233017f871753696d1c95d169614894
markusbenjamin <markus.benjamin@gmail.com>
2025-01-24 23:49:00 +0100

    Sync services.

--- services/oktopusz_keramia_temp_reporter.py
+++
@@ -1,6 +0,0 @@
-from utils.project import *
-
-system_node = JSONNodeAtURL(node_relative_path='system')
-room_temps = get_room_temps_and_humidity(False)
-oktopusz_keramia_temp = (room_temps['11']['temp']+room_temps['12']['temp'])/2/100
-system_node.write({"1":room_temps['11']['temp']/100,"2":room_temps['12']['temp']/100},'state/oktopusz_keramia')
